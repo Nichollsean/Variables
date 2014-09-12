07770ddf1fe1feb54ed62b47f3c0c7d521475f27
Nichollsean <40668@longroad.ac.uk>
2014-09-12 16:05:12 +0100

    Exercises

---
+++ Revision exercise 1.py
@@ -0,0 +1,13 @@
+#Sean Nicholls
+#12/09/14
+#Revision exercises
+
+num1= int (input("Please enter a number"))
+num2= int (input("And another"))
+num3= int (input("Another"))
+num4= int (input("One more"))
+
+ans = int(num1+num2+num3+num4)
+print(ans)
+
+
---
+++ Revision exercise 2.py
@@ -0,0 +1,11 @@
+#Sean Nicholls
+#12/09/14
+#Revison exercises
+
+num1= int(input("Feed me a number please"))
+num2= int(input("Can I have some more please"))
+num3= int(input("i like numbers, just one more"))
+
+answer= int (num1*num2/num3)
+print(answer)
+    
