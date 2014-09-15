b8abec608ba5d5019af6928518d405a777eb0e3b
Nichollsean <40668@longroad.ac.uk>
2014-09-15 21:03:08 +0100

    Just a mind refresh

---
+++ Revision Exercise 3.py
@@ -0,0 +1,12 @@
+#Sean Nicholls
+#15/09/14
+#Revision Exercises
+
+num1= int(input("Please enter a number"))
+num2= int(input("Please enter one more"))
+
+ans = num1/num2
+ans2 = num1%num2
+
+print (ans)
+print ("And a remainder of {0}" .format(ans2))
