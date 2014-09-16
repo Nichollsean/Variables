e736c20435af87ae9a092e7b7b779fc8a68dc815
Nichollsean <40668@longroad.ac.uk>
2014-09-16 10:51:34 +0100

    in class tasks

---
+++ Execise2.2.py
@@ -0,0 +1,9 @@
+#Sean Nicholls
+#16/09/14
+#Exercise 2.2
+
+degreesF= int(input("What is the temperature in degrees fahrenheit where you are:"))
+
+degreesC= (degreesF-32) * (5/9)
+
+print("The temperaure in degrees celcius is: {0:.0f}".format(degreesC))
